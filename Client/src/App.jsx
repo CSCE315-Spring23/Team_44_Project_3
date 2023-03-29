eb1c0886bf68f77820c470f02f78154a3f8cd6bd
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-28 19:16:24 -0500

    create server test. reroute login

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,27 +4,6 @@ import { HOST } from './utils/host';
 import Login from './pages/Login.jsx';
 
 function App() {
-
-	// const [orderHistory, setOrderHistory] = useState([])
-
-	// useEffect(() => {
-	// 	fetch(`${HOST}/api/orderhistory/getOrders`)
-	// 		.then(res => res.json())
-	// 		.then(data => {
-	// 			setOrderHistory(data)
-	// 		})
-	// }, [])
-
-	// return (
-	// 	<div className="App">
-	// 		Hello World!
-	// 		<ul>
-	// 			{orderHistory.map(order => (
-	// 				<li key={order.id}>{order.id}, {order.customer_name}, {order.total_cost}, {order.date}, {order.employee_id}</li>
-	// 			))}
-	// 		</ul>
-	// 	</div>
-	// );
 	return (
 		<BrowserRouter>
 			<Routes>
