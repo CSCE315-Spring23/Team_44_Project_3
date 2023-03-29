29c0773592f97d4769efc9e3e70c218f94c14abc
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-28 19:03:57 -0500

    setup react router dom

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,29 +1,37 @@
-import React, {useEffect, useState} from 'react'
-
-import { HOST } from './utils/host'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HOST } from './utils/host';
+import Login from './pages/Login.jsx';
 
 function App() {
 
-	const [orderHistory, setOrderHistory] = useState([])
+	// const [orderHistory, setOrderHistory] = useState([])
 
-	useEffect(() => {
-		fetch(`${HOST}/api/orderhistory/getOrders`)
-			.then(res => res.json())
-			.then(data => {
-				setOrderHistory(data)
-			})
-	}, [])
+	// useEffect(() => {
+	// 	fetch(`${HOST}/api/orderhistory/getOrders`)
+	// 		.then(res => res.json())
+	// 		.then(data => {
+	// 			setOrderHistory(data)
+	// 		})
+	// }, [])
 
+	// return (
+	// 	<div className="App">
+	// 		Hello World!
+	// 		<ul>
+	// 			{orderHistory.map(order => (
+	// 				<li key={order.id}>{order.id}, {order.customer_name}, {order.total_cost}, {order.date}, {order.employee_id}</li>
+	// 			))}
+	// 		</ul>
+	// 	</div>
+	// );
 	return (
-		<div className="App">
-			Hello World!
-			<ul>
-				{orderHistory.map(order => (
-					<li key={order.id}>{order.id}, {order.customer_name}, {order.total_cost}, {order.date}, {order.employee_id}</li>
-				))}
-			</ul>
-		</div>
-	)
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App
