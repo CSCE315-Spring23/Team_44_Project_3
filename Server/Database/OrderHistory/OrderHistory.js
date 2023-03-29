1bda8e6904d5129b1c526cf0624af7c5414d7c29
Kevin <kevin923.dai@gmail.com>
2023-03-28 21:43:45 -0500

    order history api

--- Server/Database/OrderHistory/OrderHistory.js
+++ Server/Database/OrderHistory/OrderHistory.js
@@ -2,10 +2,18 @@ const app = require("../../app.js");
 
 const functions = require("./OrderHistoryFunctions.js");
 
-app.get("/api/orderhistory/getOrders", (req, res) => {
+const api_path = "/api/orderhistory";
+
+app.get(api_path+"/getOrders", (req, res) => {
     functions.getOrders((result) => {
         res.send(result.rows);
     });
+});
+
+app.get(api_path+"/getOrderInformation/:id", (req, res) => {
+    functions.getOrderInformation((result) => {
+        res.send(result.rows);
+    }, req.params.id);
 });
 
 module.exports = app;
