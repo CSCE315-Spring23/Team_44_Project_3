c63e74ebf2db6d6738beb0adb14bb2ca7de21a16
Huy Lai <lai.huy@tamu.edu>
2023-03-29 01:22:10 -0500

    update index.html

--- Client/src/pages/ServerTest.jsx
+++ Client/src/pages/ServerTest.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ServerTest(){
+export default function ServerTest() {
     return (
         <>
             <h1>Server Is Connected to React!</h1>
