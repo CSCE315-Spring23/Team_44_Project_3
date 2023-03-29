21d46570138058c0df28791587600c88c71bbf8c
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-28 23:30:43 -0500

    comment index.js

--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,11 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
+
+// include database helper funcs
 const orderHistoryFuncs = require("./Database/OrderHistory/OrderHistoryFunctions");
 
+// connect to static react app
 app.use(express.static(path.join(__dirname, '../Client/dist')));
 
 app.use((req, res, next) => {
@@ -13,16 +16,20 @@ app.use((req, res, next) => {
     next();
 });
 
+/* here we will include any other app.get() endpoints that we keep in other files */
+
 app.get('/api/getOrders', (req, res) => {
     orderHistoryFuncs.getOrders((result) => {
         res.send(result.rows);
     });
 });
 
+//catch all other routes and direct them to react
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../Client/dist/index.html'));
 });
 
+//serve app
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
