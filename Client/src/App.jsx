1a89e05aec774685884050f5769dc46187ac3b82
Kevin <kevin923.dai@gmail.com>
2023-03-28 22:03:24 -0500

    exclude api urls

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HOST } from './utils/host';
 import Login from './pages/Login.jsx';
+import Order from './pages/Order.jsx';
 
 function App() {
 	return (
