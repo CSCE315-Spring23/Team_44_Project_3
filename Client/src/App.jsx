e911683d81572f55ddf839cc5dd300d517ca6790
Kevin <kevin923.dai@gmail.com>
2023-03-28 19:09:19 -0500

    order history api

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,6 +5,7 @@ import { HOST } from './utils/host'
 function App() {
 
 	const [orderHistory, setOrderHistory] = useState([])
+	const [orderInformation, setOrderInformation] = useState([])
 
 	useEffect(() => {
 		fetch(`${HOST}/api/orderhistory/getOrders`)
@@ -12,6 +13,22 @@ function App() {
 			.then(data => {
 				setOrderHistory(data)
 			})
+			.then(() => {
+
+				for(let i = 0; i < orderHistory.length; i++){
+					fetch(`${HOST}/api/orderhistory/getOrderItems/${orderHistory[i].id}`)
+						.then(res => res.json())
+						.then(data => {
+							console.log(data)
+							setOrderInformation(data)
+						})
+				}
+			})
+	}, [])
+
+	useEffect(() => {
+
+
 	}, [])
 
 	return (
@@ -20,6 +37,11 @@ function App() {
 			<ul>
 				{orderHistory.map(order => (
 					<li key={order.id}>{order.id}, {order.customer_name}, {order.total_cost}, {order.date}, {order.employee_id}</li>
+				))}
+			</ul>
+			<ul>
+				{orderInformation.map(item => (
+					<li key = {item.name}>{item.name}, {item.cost}, {item.totalsold}</li>
 				))}
 			</ul>
 		</div>
