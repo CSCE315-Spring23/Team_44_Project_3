272d70ec75949163f4f4fd6e4e0f8f1c4c011617
Huy Lai <lai.huy@tamu.edu>
2023-03-29 07:10:52 -0500

    bootleg login added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HOST } from './utils/host';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {HOST} from './utils/host';
 import Login from './pages/Login.jsx';
 import Order from './pages/Order.jsx';
 import ServerTest from './pages/ServerTest';
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<BrowserRouter basename="/">
 			<Routes>
-				<Route path="/" element={<ServerTest />} />
+				<Route path="/" element={<Login />} />
 				<Route path="/Login" element={<Login />} />
 				<Route path="/Order" element={<Order />} />
 			</Routes>
