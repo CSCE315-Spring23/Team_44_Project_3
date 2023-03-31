564e856dc3d523323008908c27ebf386d74b181a
stevenmao27 <steven2003mao@gmail.com>
2023-03-31 15:56:37 -0500

    add db object

--- Server/api/Database/Order.js
+++ Server/api/Database/Order.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const orderRouter = express.Router();
 const apiPath = "/api/order";
+const db = require('./Info/DatabaseConnect.js');
 const { ORDER_ITEM_DATABASE, MENU_ITEM_DATABASE, SOLD_ITEM_DATABASE } = require('./Info/DatabaseNames.js');
 
 orderRouter.get(apiPath + "/getMenu", async (req, res) => {
-    try{
+    try {
         const response = await db.query(`SELECT * FROM ${MENU_ITEM_DATABASE}`);
         res.send(response.rows);
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
 });
