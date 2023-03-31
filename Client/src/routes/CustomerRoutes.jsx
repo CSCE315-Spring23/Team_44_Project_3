15f56a20c02ab41a64a5f7584ff5d4ca348272e7
Huy Lai <lai.huy@tamu.edu>
2023-03-31 09:49:00 -0500

    add entree page

--- Client/src/routes/CustomerRoutes.jsx
+++ Client/src/routes/CustomerRoutes.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Route, Routes, Outlet } from "react-router-dom";
+import {Route, Routes, Outlet} from "react-router-dom";
 import Sauces from "../pages/customer/sauces";
+import Entree from "../pages/customer/entree";
 import CustomerOrder from '../pages/customer/CustomerOrder';
 
 export default function CustomerRoutes() {
     return (
         <Routes>
             <Route path="/CustomerOrder" element={<Outlet />} >
-                <Route path='' element={<CustomerOrder/>}/>
+                <Route path='' element={<CustomerOrder />} />
+                <Route path="entrées" element={<Entree />} />
                 <Route path="sauces" element={<Sauces />} />
             </Route>
         </Routes>
