f4dfedbc31a2c8303baa4ef2029e7459a614b481
stevenmao27 <steven2003mao@gmail.com>
2023-03-31 16:20:40 -0500

    basic comments and debugging statements

--- Server/api/Database/Order.js
+++ Server/api/Database/Order.js
@@ -4,9 +4,16 @@ const apiPath = "/api/order";
 const db = require('./Info/DatabaseConnect.js');
 const { ORDER_ITEM_DATABASE, MENU_ITEM_DATABASE, SOLD_ITEM_DATABASE } = require('./Info/DatabaseNames.js');
 
+/*
+    Query Database for Menu Items
+    @param: None
+    @return: Something
+*/
 orderRouter.get(apiPath + "/getMenu", async (req, res) => {
+    console.log('hello there!');
     try {
         const response = await db.query(`SELECT * FROM ${MENU_ITEM_DATABASE}`);
+        console.log(response.rows);
         res.send(response.rows);
     } catch (err) {
         console.log(err);
