a7ec3605c0d37953148c140a203804d264912dd9
stevenmao27 <steven2003mao@gmail.com>
2023-03-31 15:54:51 -0500

    create getMenu

--- Server/api/Database/Order.js
+++ Server/api/Database/Order.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const orderRouter = express.Router();
-const functions = require("./QueryFunctions/OrderFunctions.js");
 const apiPath = "/api/order";
+const { ORDER_ITEM_DATABASE, MENU_ITEM_DATABASE, SOLD_ITEM_DATABASE } = require('./Info/DatabaseNames.js');
+
+orderRouter.get(apiPath + "/getMenu", async (req, res) => {
+    try{
+        const response = await db.query(`SELECT * FROM ${MENU_ITEM_DATABASE}`);
+        res.send(response.rows);
+    } catch(err){
+        console.log(err);
+    }
+});
