01c9367119b3bf643046098e22da78f26433d288
Huy Lai <lai.huy@tamu.edu>
2023-03-31 08:54:04 -0500

    add being able to change pages from customerorder

--- Client/src/components/CategoryItem.jsx
+++ Client/src/components/CategoryItem.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import images from '../utils/imageImport';
+import React, {useEffect, useState} from 'react';
 
 /**
  * 
@@ -11,14 +10,16 @@ export default function CategoryItem(props) {
     return (
 
         <li role="listitem">
-            <button data-cy={"\"" + item.title + "\""} className="menuButton">
-                <div className="image">
-                    <img alt={item.title} src={item.src} data-cy="ProductImageAvailable" aria-hidden="true" className="menuIMG" />
-                </div>
-                <div className="name">
-                    <h3 className="name">{item.title}</h3>
-                </div>
-            </button>
+            <a href={"/customerorder/" + item.title.toLowerCase()} className="menuLink">
+                <button data-cy={"\"" + item.title + "\""} className="menuButton">
+                    <div className="image">
+                        <img alt={item.title} src={item.src} data-cy="ProductImageAvailable" aria-hidden="true" className="menuIMG" />
+                    </div>
+                    <div className="name">
+                        <h3 className="name">{item.title}</h3>
+                    </div>
+                </button>
+            </a>
         </li>
     );
 }
