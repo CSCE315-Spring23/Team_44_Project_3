dd499fd7508f340316f0f6c550046e2b7d7a24b2
stevenmao27 <steven2003mao@gmail.com>
2023-03-31 19:10:06 -0500

    complete /postOrder function

--- Server/api/Database/Order.js
+++ Server/api/Database/Order.js
@@ -2,7 +2,7 @@ const express = require('express');
 const orderRouter = express.Router();
 const apiPath = "/api/order";
 const db = require('./Info/DatabaseConnect.js');
-const { ORDER_ITEM_DATABASE, MENU_ITEM_DATABASE, SOLD_ITEM_DATABASE } = require('./Info/DatabaseNames.js');
+const { ORDER_ITEM_DATABASE, MENU_ITEM_DATABASE, SOLD_ITEM_DATABASE, INVENTORY_DATABASE } = require('./Info/DatabaseNames.js');
 
 /*
     Query Database for Menu Items
@@ -22,14 +22,28 @@ orderRouter.get(apiPath + "/getMenu", async (req, res) => {
 
 /*
     Send Order to Database
-    @param: JSON object: {customerName: string, totalCost: float, employeeID: int, items: list[{id: int, quantity: int}]}
+    @param: JSON object: {customerName: string, totalCost: float OR string, employeeID: int, items: list[{id: int, quantity: int}]}
         ex: { "customerName": "John Doe", "totalCost": 12.34, "employeeID": 1, "items": [ { "id": 5, "quantity": 2 }, ... ] }
     @return: none
 */
 orderRouter.post(apiPath + "/postOrder", async (req, res) => {
     try {
+        // extract data
+        const { customerName, totalCost, employeeID, items } = req.body;
+        const currentDate = new Date();
+        const formattedDateTime = currentDate.toISOString().replace('T', ' ').split('.')[0];
+
+        // query next orderitem id
+        let id = await db.query(`SELECT MAX(id) FROM ${ORDER_ITEM_DATABASE}`);
+        id = id.rows[0].max + 1;
+
+        // insert receipt into orderitem
+        await db.query(`INSERT INTO ${ORDER_ITEM_DATABASE} VALUES (${id}, '${customerName}', '${totalCost}', '${formattedDateTime}', ${employeeID})`);
+
+        res.status(200).send("Order Received");
     } catch (err) {
-        console.log(err);
+        console.log("CAUGHT ERROR: " + err);
+        res.status(500).send("Error: " + err);
     }
 });
 
