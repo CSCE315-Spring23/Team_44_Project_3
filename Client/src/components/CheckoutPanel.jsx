f4b3fe686c338e3fee6b5a661a1090d3367a542a
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-31 21:29:24 -0500

    fix link not found

--- Client/src/components/CheckoutPanel.jsx
+++ Client/src/components/CheckoutPanel.jsx
@@ -1,10 +1,10 @@
 import React, {useState, useEffect, useContext} from "react";
-
+import { Link } from "react-router-dom";
 
 export default function CheckoutPanel(props){
     return(
         <div className="checkoutPanel">
-            
+
             <Link className='empNavButton'>Menu Board</Link>
         </div>
     );
