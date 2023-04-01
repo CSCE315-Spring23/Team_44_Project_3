f67dbff14286c7061c64192188cf80e2e614fbf7
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-31 20:18:19 -0500

    render order buttons

--- Client/src/components/OrderItemPane.jsx
+++ Client/src/components/OrderItemPane.jsx
@@ -3,36 +3,39 @@ import OrderItemButton from "./OrderItemButton";
 import { endpoints } from "../utils/apiEndpoints";
 import { HOST } from "../utils/host";
 
-export default function OrderItemPane(){
+export default function OrderItemPane() {
     //set a react state to contain the json of menu items
-    const [menuItems, setMenuItems] = useState(null);
-
+    const [menuButtons, setMenuButtons] = useState(null);
     //fetch the menu items from the api
-    useEffect(() =>{
-        setMenuItems(null);
+    useEffect(() => {
+        setMenuButtons(null);
         const url = HOST + endpoints.getMenu;
         fetch(url, {
             method: "GET"
         })
-        .then(response => {
-            if(!response.ok){
-                throw new Error("Network Response Not OK");
-            }
-            return response.json();
-        })
-        .then(data => {
-            setMenuItems(data);
-            console.log(menuItems);
-        })
-        .catch(error => {
-            console.error("Could not fetch menu items from " + url);
-        });
-        
-    },[]);
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network Response Not OK");
+                }
+                return response.json();
+            })
+            .then(data => {
+                //create the map of all the menu buttons to render
+                const menBtns = data.map((element) =>
+                    <OrderItemButton itemName={element["name"]} />
+                );
+                setMenuButtons(menBtns);
+            })
+            .catch(error => {
+                console.error("Could not fetch menu items from " + url);
+            });
 
-    return(
+    }, []);
+
+    return (
         <div className="orderItemPane">
             <OrderItemButton itemName="Button1"></OrderItemButton>
+            {menuButtons}
         </div>
     );
 }
