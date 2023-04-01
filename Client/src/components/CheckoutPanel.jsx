666cb1141e6356e288e556f77f8143d5c4c590c8
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-31 20:31:44 -0500

    start checkout panel

--- Client/src/components/CheckoutPanel.jsx
+++ Client/src/components/CheckoutPanel.jsx
@@ -3,7 +3,8 @@ import React, {useState, useEffect, useContext} from "react";
 
 export default function CheckoutPanel(props){
     return(
-        <>
-        </>
+        <div className="checkoutPanel">
+            <p>checkout text</p>
+        </div>
     );
 }
