23f35b71152ad4ae023a9d16c729b418191d9cea
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-31 21:26:47 -0500

    more checkout panel

--- Client/src/components/CheckoutPanel.jsx
+++ Client/src/components/CheckoutPanel.jsx
@@ -4,7 +4,8 @@ import React, {useState, useEffect, useContext} from "react";
 export default function CheckoutPanel(props){
     return(
         <div className="checkoutPanel">
-            <p>checkout text</p>
+            
+            <Link className='empNavButton'>Menu Board</Link>
         </div>
     );
 }
