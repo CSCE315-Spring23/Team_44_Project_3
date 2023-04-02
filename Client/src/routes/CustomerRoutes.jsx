faad97c18c9aab853b8aadc8f9eb195121c8b43e
Huy Lai <lai.huy@tamu.edu>
2023-04-02 05:03:00 -0500

    add salads page

--- Client/src/routes/CustomerRoutes.jsx
+++ Client/src/routes/CustomerRoutes.jsx
@@ -3,8 +3,9 @@ import {Route, Routes, Outlet} from "react-router-dom";
 import CustomerOrder from '../pages/customer/CustomerOrder';
 import Beverage from "../pages/customer/beverages"
 import Entree from "../pages/customer/entree";
-import Sauces from "../pages/customer/sauces";
 import Treats from "../pages/customer/treats";
+import Salad from "../pages/customer/salads";
+import Sauces from "../pages/customer/sauces";
 
 export default function CustomerRoutes() {
     return (
@@ -13,8 +14,9 @@ export default function CustomerRoutes() {
                 <Route path="order" element={<CustomerOrder />} />
                 <Route path="order/beverages" element={<Beverage />} />
                 <Route path="order/entrées" element={<Entree />} />
-                <Route path="order/sauces" element={<Sauces />} />
                 <Route path="order/treats" element={<Treats />} />
+                <Route path="order/salads" element={<Salad />} />
+                <Route path="order/sauces" element={<Sauces />} />
             </Route>
         </Routes>
     );
