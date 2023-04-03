0978cb7ca17c8d0f1db51f5f8a8dbefb8b1d4ac2
Shreeman <physics24@tamu.edu>
2023-04-03 02:23:18 -0500

    Local Changes

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,9 +1,20 @@
 import React, {useEffect, useState} from 'react';
+import chickFilA from '../assets/chickFilA.png';
+import '../styles/login.css';
 
 function Login() {
+
+    const handleLoginButtonClick = (event) => {
+
+    }
+
     return(
         <div>
-            <h1>Test Login Page</h1>
+            <img className='logo' src={chickFilA} />
+            <div>
+                <button className='orderButton'>Order</button>
+                <button className='employeeButton'>Employee Login</button>
+            </div>
         </div>
     );
 }
