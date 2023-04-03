c78dc3beb8901f11398f68685b278b4010756485
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-04-03 09:30:21 -0500

    add logout button

--- Client/src/components/EmployeeNav.jsx
+++ Client/src/components/EmployeeNav.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function av(props) {
 
     const isManager = props.isManager;
+    const navigate = useNavigate();
+    const handleLogout = () => {
+
+        navigate('/Login');
+    }
+
     if (isManager) {
         return (
             <nav className='empNav'>
@@ -13,6 +20,7 @@ export default function av(props) {
                 <Link className='empNavButton' to='/employee/menu'>Menu</Link>
                 <Link className='empNavButton'>Reports</Link>
                 <Link className='empNavButton'>Menu Board</Link>
+                <button onClick={e => handleLogout()} className='empNavLogout'>Logout</button>
             </nav>
         );
     }
@@ -21,6 +29,7 @@ export default function av(props) {
             <nav className='empNav'>
                 <Link className='empNavButton' to='/employee/order'>Order</Link>
                 <Link className='empNavButton' to='/employee/orderHistory'>Order History</Link>
+                <button onClick={e => handleLogout()} className='empNavLogout'>Logout</button>
             </nav>
         );
     }
