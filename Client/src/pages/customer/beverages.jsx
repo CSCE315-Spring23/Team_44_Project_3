730449fa1e99e79ca7743cc1892b42b09440e989
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-04-04 14:51:26 -0500

    add drink ids

--- Client/src/pages/customer/beverages.jsx
+++ Client/src/pages/customer/beverages.jsx
@@ -7,19 +7,19 @@ import {useNavigate} from 'react-router-dom';
 import beveragesImages from '../../utils/beverageImages';
 
 const menu = [
-    {title: "Bottled Watter", src: beveragesImages.beverage0, key: 0},
-    {title: "M. Unsweetened Iced Tea", src: beveragesImages.beverage1, key: 1},
-    {title: "L. Unsweetened Iced Tea", src: beveragesImages.beverage2, key: 2},
-    {title: "M. Sweet Iced Tea", src: beveragesImages.beverage3, key: 3},
-    {title: "L. Sweet Iced Tea", src: beveragesImages.beverage4, key: 4},
-    {title: "M. Lemonade", src: beveragesImages.beverage5, key: 5},
-    {title: "L. Lemonade", src: beveragesImages.beverage6, key: 6},
-    {title: "M. Sunjoy", src: beveragesImages.beverage7, key: 7},
-    {title: "L. Sunjoy", src: beveragesImages.beverage8, key: 8},
-    {title: "M. Soft Drink", src: beveragesImages.beverage9, key: 9},
-    {title: "L. Soft Drink", src: beveragesImages.beverage10, key: 10},
-    {title: "Original Cold Brew Iced Coffee", src: beveragesImages.beverage11, key: 11},
-    {title: "Vanilla Cold Brew Iced Coffee", src: beveragesImages.beverage12, key: 12},
+    {title: "Bottled Watter", src: beveragesImages.beverage0, key: 0, id: 20},
+    {title: "M. Unsweetened Iced Tea", src: beveragesImages.beverage1, key: 1, id: 21},
+    {title: "L. Unsweetened Iced Tea", src: beveragesImages.beverage2, key: 2, id: 22},
+    {title: "M. Sweet Iced Tea", src: beveragesImages.beverage3, key: 3, id: 23},
+    {title: "L. Sweet Iced Tea", src: beveragesImages.beverage4, key: 4, id: 24},
+    {title: "M. Lemonade", src: beveragesImages.beverage5, key: 5, id: 25},
+    {title: "L. Lemonade", src: beveragesImages.beverage6, key: 6, id: 26},
+    {title: "M. Sunjoy", src: beveragesImages.beverage7, key: 7, id: 27},
+    {title: "L. Sunjoy", src: beveragesImages.beverage8, key: 8, id: 28},
+    {title: "M. Soft Drink", src: beveragesImages.beverage9, key: 9, id: 29},
+    {title: "L. Soft Drink", src: beveragesImages.beverage10, key: 10, id: 30},
+    {title: "Original Cold Brew Iced Coffee", src: beveragesImages.beverage11, key: 11, id: 31},
+    {title: "Vanilla Cold Brew Iced Coffee", src: beveragesImages.beverage12, key: 12, id: 32},
 ];
 
 const menuList = menu.map(item =>
