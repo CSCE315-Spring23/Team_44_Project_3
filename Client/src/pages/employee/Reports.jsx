d8749168599982640e7649fb0afff05486650171
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-04-11 15:10:08 -0500

    add nav to reports

--- Client/src/pages/employee/Reports.jsx
+++ Client/src/pages/employee/Reports.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import EmployeeNav from "../../components/EmployeeNav";
 
 export default function Reports(){
     return(
-        <></>
+        <div className="empOrderPage">
+            <EmployeeNav></EmployeeNav>
+        <div id="reportsSelection">
+            
+        </div>
+        </div>
     );
 }
