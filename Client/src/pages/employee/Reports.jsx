36fffd39688c7a225e81ba4820be81187f7eff11
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-04-11 15:57:06 -0500

    add routes to report btns

--- Client/src/pages/employee/Reports.jsx
+++ Client/src/pages/employee/Reports.jsx
@@ -8,11 +8,11 @@ export default function Reports() {
         <div className="empOrderPage">
             <EmployeeNav></EmployeeNav>
             <div id="reportsSelection">
-                <Link className='reportBtn'>Sales Report</Link>
-                <Link className='reportBtn'>Excess Report</Link>
-                <Link className='reportBtn'>XZ Report</Link>
-                <Link className='reportBtn'>Restock Report</Link>
-                <Link className='reportBtn'>Sales Together Report</Link>
+                <Link className='reportBtn' to='sales'>Sales Report</Link>
+                <Link className='reportBtn' to='excess'>Excess Report</Link>
+                <Link className='reportBtn' to='XZ'>XZ Report</Link>
+                <Link className='reportBtn' to='restock'>Restock Report</Link>
+                <Link className='reportBtn' to='salestogether'>Sales Together Report</Link>
             </div>
         </div>
     );
