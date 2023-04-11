8dccdf905fecfa1f2607d1955a2781f2a7f7d499
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-04-11 15:32:35 -0500

    add routes

--- Client/src/pages/employee/Reports.jsx
+++ Client/src/pages/employee/Reports.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import EmployeeNav from "../../components/EmployeeNav";
+import { Link } from "react-router-dom";
 
-export default function Reports(){
-    return(
+export default function Reports() {
+    return (
         <div className="empOrderPage">
             <EmployeeNav></EmployeeNav>
-        <div id="reportsSelection">
-            
-        </div>
+            <div id="reportsSelection">
+                <Link className='reportBtn'>Sales Report</Link>
+            </div>
         </div>
     );
 }
--- Client/src/routes/EmployeeRoutes.jsx
+++ Client/src/routes/EmployeeRoutes.jsx
@@ -6,6 +6,11 @@ import OrderHistory from "../pages/employee/OrderHistory";
 import Inventory from "../pages/employee/Inventory";
 import Menu from "../pages/employee/Menu";
 import Reports from "../pages/employee/Reports";
+import SalesRep from "../pages/employee/SalesRep";
+import ExcessRep from "../pages/employee/ExcessRep";
+import SalesTogetherRep from "../pages/employee/SalesTogetherRep";
+import XZRep from "../pages/employee/XZRep";
+import RestockRep from "../pages/employee/RestockRep";
 
 export default function EmployeeRoutes() {
     return (
@@ -13,10 +18,16 @@ export default function EmployeeRoutes() {
             <Route path="/employee">
                 <Route path='order' element={<Order />} />
                 <Route path='orderHistory' element={<OrderHistory />} />
-                <Route path = 'inventory' element={<Inventory />} />
-                <Route path = 'menu' element={<Menu />} />
-                <Route path = 'reports' element={<Reports />} />
+                <Route path='inventory' element={<Inventory />} />
+                <Route path='menu' element={<Menu />} />
+                <Route path='reports' element={<Reports />} >
+                    <Route path="sales" element={<SalesRep/>}></Route>
+                    <Route path="restock" element={<RestockRep/>}></Route>
+                    <Route path="excess" element={<ExcessRep/>}></Route>
+                    <Route path="XZ" element={<XZRep/>}></Route>
+                    <Route path="salestogether" element={<SalesTogetherRep/>}></Route>
+                </Route>
             </Route>
-        </Routes>
+        </Routes >
     );
 }
