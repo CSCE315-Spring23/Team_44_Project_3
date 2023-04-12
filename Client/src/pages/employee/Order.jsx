8717e503bd5d3698073bfc32a79f305607fa08b7
Kevin <kevin923.dai@gmail.com>
2023-04-12 15:04:49 -0500

    remove single item from cart manager view

--- Client/src/pages/employee/Order.jsx
+++ Client/src/pages/employee/Order.jsx
@@ -41,12 +41,27 @@ export default function Order(props) {
         localStorage.setItem('cart', JSON.stringify({ total: [0], items: {} }));
     }
 
+    const removeFromCart = (itemName) => {
+        console.log("remove from cart clicked ->", itemName)
+
+        let newCart = { ...cart };
+        newCart.total[0] -= newCart.items[itemName][1];
+        if(newCart.items[itemName][0] > 1){
+            newCart.items[itemName][0] -= 1;
+        }
+        else{
+            delete newCart.items[itemName];
+        }
+        setCart(newCart);
+        localStorage.setItem('cart', JSON.stringify(newCart));
+    }
+
     const isManager = props.isManager;
     return (
         <div className="empOrderPage">
             <EmployeeNav isManager={isManager}></EmployeeNav>
             <OrderItemPane addToCart={addToCart}></OrderItemPane>
-            <CheckoutPanel cart={cart} emptyCart={emptyCart} />
+            <CheckoutPanel cart={cart} emptyCart={emptyCart} removeFromCart={removeFromCart}/>
         </div>
     );
 }
