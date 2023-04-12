6b485104c0174dfacd4a3759adff7a854a731e0a
Huy Lai <lai.huy@tamu.edu>
2023-04-12 02:34:34 -0500

    add ability to remove items

--- Client/src/components/AddMenuItem.jsx
+++ Client/src/components/AddMenuItem.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { HOST } from '../utils/host';
+import React, {useEffect, useState} from 'react';
+import {HOST} from '../utils/host';
 
 /**
  * 
@@ -14,7 +14,7 @@ export default function AddMenuItem(props) {
         let order = localStorage.getItem('curOrder');
         console.log("order before changes: ", order);
         if (!order) {
-            order = { total: [0], items: [] };
+            order = {total: [0], items: []};
         }
         else {
             order = JSON.parse(order);
@@ -28,7 +28,7 @@ export default function AddMenuItem(props) {
                 menu.forEach((arrItem) => {
                     if (arrItem.id == curID) {
                         order.total[0] += Number(arrItem.cost);
-                        order.items.push({"id" : arrItem.id, "quantity" : 1});
+                        order.items.push({"id": arrItem.id, "quantity": 1});
                     }
                 });
             });
@@ -39,7 +39,42 @@ export default function AddMenuItem(props) {
             menu.forEach((arrItem) => {
                 if (arrItem.id == item.id) {
                     order.total[0] += Number(arrItem.cost);
-                    order.items.push({"id" : arrItem.id, "quantity" : 1});
+                    order.items.push({"id": arrItem.id, "quantity": 1});
+                }
+            });
+        }
+        console.log(order);
+        localStorage.setItem('curOrder', JSON.stringify(order));
+    }
+
+    function removeOrder(item) {
+        let order = localStorage.getItem('curOrder');
+        if (!order)
+            return;
+        else
+            order = JSON.parse(order);
+
+        // add to the order object
+        const menu = JSON.parse(localStorage.getItem('menu'));
+        //handle meals
+        if (item.ids) {
+            item.ids.forEach((curID) => {
+                console.log("meal id: ", curID);
+                menu.forEach((arrItem) => {
+                    if (arrItem.id == curID) {
+                        order.total[0] -= Number(arrItem.cost);
+                        order.items.pop({"id": arrItem.id, "quantity": 1});
+                    }
+                });
+            });
+        }
+        //handle all other items
+        if (item.id) {
+            console.log("clicked itemid: ", item.id);
+            menu.forEach((arrItem) => {
+                if (arrItem.id == item.id) {
+                    order.total[0] -= Number(arrItem.cost);
+                    order.items.pop({"id": arrItem.id, "quantity": 1});
                 }
             });
         }
