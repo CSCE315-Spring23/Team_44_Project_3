36a8be7e42ee5febc3ee688dde5fbad97ece07b0
Steven Mao <steven2003mao@gmail.com>
2023-04-17 15:58:34 -0500

    login endpoint debugging

--- Server/api/Database/Login.js
+++ Server/api/Database/Login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const loginRouter = express.Router();
 const apiPath = "/api/login";
+const db = require('./Info/DatabaseConnect.js');
 
 // checks if valid user
 // returns user if valid
@@ -12,10 +13,13 @@ loginRouter.post(apiPath + "/", async (req, res) => {
 
     // check if user exists
     if (pin !== '') {
-
+        let response = await db.query(`SELECT EXISTS (SELECT 1 FROM employee WHERE pin = '${pin}')`)
+        console.log(response);
     } else {
-
+        let response = await db.query(`SELECT EXISTS (SELECT 1 FROM employee WHERE email = '${email}')`)
+        console.log(response);
     }
+    
 
     res.send({ isValidUser, isManager });
 });
