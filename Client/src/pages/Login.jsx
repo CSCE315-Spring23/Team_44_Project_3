318e4ebd17f744d10e56a627a5b0e27254c613aa
Huy Lai <lai.huy@tamu.edu>
2023-04-19 06:40:55 -0500

    move oauth sign in to the employee only section

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import EmployeeNav from "../components/EmployeeNav";
+import React, {useEffect, useState} from "react";
 
-import { useNavigate } from 'react-router-dom';
-import { endpoints } from "../utils/apiEndpoints";
-import { HOST } from "../utils/host";
-import logo from '../assets/CFA Banner.svg'
+import {useNavigate} from "react-router-dom";
+import logo from "../assets/CFA Banner.svg";
+import {endpoints} from "../utils/apiEndpoints";
+import {HOST} from "../utils/host";
 
 import jwt_decode from "jwt-decode";
 
@@ -18,25 +17,25 @@ export default function Login(props) {
     // * @returns: resolves to true if user found and updated localstorage, false otherwise
     async function OAUTH(params) {
         const url = HOST + endpoints.loginAPI;
-        const { email = '', pin = '' } = params;
+        const {email = "", pin = ""} = params;
 
         const response = await fetch(url, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                "Accept": "application/json",
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(params)
         })
-        
+
         const data = await response.json();
 
         if (data.isValidUser) {
-            localStorage.setItem('employeeId', data.id);
-            localStorage.setItem('name', data.name);
-            localStorage.setItem('email', email);
-            localStorage.setItem('pin', pin);
-            localStorage.setItem('isManager', data.isManager);
+            localStorage.setItem("employeeId", data.id);
+            localStorage.setItem("name", data.name);
+            localStorage.setItem("email", email);
+            localStorage.setItem("pin", pin);
+            localStorage.setItem("isManager", data.isManager);
 
             return true;
         } else {
@@ -47,46 +46,46 @@ export default function Login(props) {
     function handleGoogleLogin(response) {
         const decodedResponse = jwt_decode(response.credential);
         const email = decodedResponse.email;
-        
+
         // if oauth successful, log them in
-        OAUTH({ email })
+        OAUTH({email})
             .then(isValid => {
-                if (isValid) navigate('/employee/order');
+                if (isValid) navigate("/employee/order");
                 else setErrorMessage("Invalid Google User");
             });
     }
 
     function handlePINLogin() {
-        const employeePin = document.getElementById('pass').value;
+        const employeePin = document.getElementById("pass").value;
 
         if (!employeePin || employeePin.length < 4)
             return;
-        
+
         // if pin is valid, log them in
-        OAUTH({ pin: employeePin })
+        OAUTH({pin: employeePin})
             .then(isValid => {
-                if (isValid) navigate('/employee/order');
+                if (isValid) navigate("/employee/order");
                 else setErrorMessage("Invalid PIN");
             });
     }
 
     // set up google oauth button and behavior
     useEffect(() => {
-        const script = document.createElement('script');
+        const script = document.createElement("script");
         script.src = "https://accounts.google.com/gsi/client";
         script.async = true;
         document.body.appendChild(script);
 
         google.accounts.id.initialize({
-            client_id: '494495949926-om1cjij44iie2585is6tk7n98aurqso6.apps.googleusercontent.com',
+            client_id: "494495949926-om1cjij44iie2585is6tk7n98aurqso6.apps.googleusercontent.com",
             callback: handleGoogleLogin
         });
 
         google.accounts.id.renderButton(
-            document.getElementById('google-signin'),
+            document.getElementById("google-signin"),
             {
-                theme: 'outline',
-                size: 'large'
+                theme: "outline",
+                size: "large"
             }
         );
     });
@@ -102,15 +101,16 @@ export default function Login(props) {
                 <a className="order-link" href="/customer/order">Order Now</a>
             </div>
 
-            <div id='google-signin'></div>
 
             <div className="login-wrapper">
                 <div className="login-text">
                     {errorMessage ? <p>{errorMessage}</p> : <p>Employees Only</p>}
                 </div>
 
+                <div id="google-signin"></div>
+
                 <div>
-                    <input type="password" id="pass" name="password" className="sign-in-field" placeholder="Enter PIN Number" onKeyDown={(e) => {if (e.key == 'Enter') handlePINLogin();}}/>
+                    <input type="password" id="pass" name="password" className="sign-in-field" placeholder="Enter PIN Number" onKeyDown={(e) => {if (e.key == "Enter") handlePINLogin();}} />
                     <input type="submit" value="Sign In" onClick={handlePINLogin} className="sign-in-button" />
                 </div>
             </div>
