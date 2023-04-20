ebb804ef22b001f7b64dda95b52413bbbdd3850f
Huy Lai <lai.huy@tamu.edu>
2023-04-20 12:39:15 -0500

    update run script and readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Project 3
+
+## How to run
+### PowerSheel
+
+Run using `.\run_dev.ps1 run` 
+Install using `.\run_dev.ps1 install`
+
+### Bash
+
+Run using `make run`  
+Install using `make install`
--- run_dev.ps1
+++ run_dev.ps1
@@ -1,4 +1,36 @@
-cd .\Client
-npm run build
-cd ..\Server
-npm run dev
+$ErrorActionPreference = "Stop"
+
+function Run {
+	Set-Location -Path "./Client"
+	npm run build
+	Set-Location -Path "../Server"
+	npm run dev
+	Set-Location -Path "../"
+}
+
+function Install {
+	Set-Location -Path "./Client"
+	npm install
+	Set-Location -Path "../Server"
+	npm install
+	Set-Location -Path "../"
+}
+
+$targets = @{
+	"run"     = { Run }
+	"install" = { Install }
+}
+
+if ($args.Count -eq 0) {
+	Write-Host "Usage: `n`make target`n`nTargets:`n`n$($targets.Keys -join "`n")"
+	exit 1
+}
+
+$target = $args[0]
+
+if (-not $targets.ContainsKey($target)) {
+	Write-Error "Target not found: $target"
+	exit 1
+}
+
+Invoke-Command $targets[$target]
