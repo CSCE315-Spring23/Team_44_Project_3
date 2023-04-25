54a1de8b7355dca1659c3ce0284738881f4ce4a5
Kevin <kevin923.dai@gmail.com>
2023-04-24 20:50:06 -0500

    menu board set up with name and cost, css needed

--- Client/src/pages/employee/MenuBoard.jsx
+++ Client/src/pages/employee/MenuBoard.jsx
@@ -13,12 +13,156 @@ export default function MenuBoard(props) {
     const [treats, setTreats] = useState([]);
     const [beverages, setBeverages] = useState([]);
     const [sauces, setSauces] = useState([]);
+    const [seasonal, setSeasonal] = useState([]);
 
+    const meal = 4.28;
+
+    useEffect(() => {
+        const url = HOST + endpoints.getMenu;
+        fetch(url,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network Response Not OK");
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("data from MenuBoard: ", data);
+                for (let i = 0; i < data.length; i++) {
+                    switch (data[i].category) {
+                        case "entree":
+                            setEntrees(entrees => [...entrees, data[i]]);
+                            break;
+                        case "salad":
+                            setSalads(salads => [...salads, data[i]]);
+                            break;
+                        case "side":
+                            setSides(sides => [...sides, data[i]]);
+                            break;
+                        case "treat":
+                            setTreats(treats => [...treats, data[i]]);
+                            break;
+                        case "beverage":
+                            setBeverages(beverages => [...beverages, data[i]]);
+                            break;
+                        case "sauce":
+                            setSauces(sauces => [...sauces, data[i]]);
+                            break;
+                        default:
+                            setSeasonal(seasonal => [...seasonal, data[i]]);
+                            break;
+                    }
+                }
+            })
+            .catch(error => {
+                console.error("Could not fetch menu items from " + url);
+            });
+    }, []);
 
     return (
         <div className="menuBoardPage">
             <div className="menuBoardTitle">
                 <h1>Menu</h1>
+            </div>
+
+            <div className="menuBoard">
+                <div className="menuBoardEntrees">
+                    <div className="menuBoardCate">
+                    <h2>Entrees</h2>
+                    </div>
+                    <div className="menuBoardList">
+                        {entrees.map((item, index) => (
+                            <div className="menuBoardItem" key={index}>
+                                <h3>{index + 1}. {item.name}: ${item.cost}</h3>
+                                <p>Meal: ${(parseFloat(item.cost) + meal).toFixed(2)}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="menuBoardSS">
+                    <div className="menuBoardSalads">
+                    <div className="menuBoardCate">
+                        <h2>Salads</h2>
+                        </div>
+                        <div className="menuBoardList">
+                            {salads.map((item, index) => (
+                                <div className="menuBoardItem" key={index}>
+                                    <h3>{index + 1}. {item.name}: ${item.cost}</h3>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="menuBoardSides">
+                    <div className="menuBoardCate">
+                        <h2>Sides</h2>
+                        </div>
+                        <div className="menuBoardList">
+                            {sides.map((item, index) => (
+                                <div className="menuBoardItem" key={index}>
+                                    <h3>{index + 1}. {item.name}: ${item.cost}</h3>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="menuBoardTreats">
+                <div className="menuBoardCate">
+                    <h2>Treats</h2>
+                    </div>
+                    <div className="menuBoardList">
+                        {treats.map((item, index) => (
+                            <div className="menuBoardItem" key={index}>
+                                <h3>{index + 1}. {item.name}: ${item.cost}</h3>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="menuBoardBeverages">
+                    <div className="menuBoardCate">
+                    <h2>Beverages</h2>
+                    </div>
+                    <div className="menuBoardList">
+                        {beverages.map((item, index) => (
+                            <div className="menuBoardItem" key={index}>
+                                <h3>{index + 1}. {item.name}: ${item.cost}</h3>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="menuBoardSauces">
+                    <div className="menuBoardCate">
+                    <h2>Sauces</h2>
+                    </div>
+                    <div className="menuBoardList">
+                        {sauces.map((item, index) => (
+                            <div className="menuBoardItem" key={index}>
+                                <h3>{index + 1}. {item.name}: ${item.cost}</h3>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="menuBoardSeasonal">
+                    <div className="menuBoardCate">
+                    <h2>Seasonal</h2>
+                    </div>
+                    <div className="menuBoardList">
+                        {seasonal.map((item, index) => (
+                            <div className="menuBoardItem" key={index}>
+                                <h3>{index + 1}. {item.name}: ${item.cost}</h3>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
