3853d33253680ebad598c2e3e839bf00491263ee
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2023-04-26 09:53:40 -0500

    Create PageProtector.jsx
    Please work

---
+++ Client/src/components/PageProtector.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+export default function PageProtector({children}) {
+    const [isLoading, setIsLoading] = useState(true);
+    const nav = useNavigate();
+    console.log("pageProtector");
+    useEffect(() => {
+        if (localStorage.getItem("employeeId") != null) {
+            setIsLoading(false);
+        }
+        else {
+            console.log("pageprotector nav to login");
+            nav("/");
+            setIsLoading(true);
+        }
+    }, []);
+
+    if (isLoading) {
+        return (
+            <p>Page Loading Or No Authorization</p>
+        );
+    }
+
+    return children;
+}
