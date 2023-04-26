8b1819ac758273d19e96ccc5a911b30488150c72
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-04-25 22:15:18 -0500

    page protect employee order

--- Client/src/components/pageProtector.jsx
+++ Client/src/components/pageProtector.jsx
@@ -1,12 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function pageProtector({ children }) {
+export default function PageProtector({ children }) {
     const [isLoading, setIsLoading] = useState(true);
-
+    const nav = useNavigate();
+    console.log("pageProtector");
     useEffect(() => {
         //TODO Check auth state and update is loading
-    }, []);
+        if(localStorage.getItem("employeeId") != null){
+            console.log("not loading");
+            setIsLoading(false);
+        }
+        else{
+            console.log("loading");
+            nav("/");
+            setIsLoading(true);
+        }
+    },[]);
 
     if (isLoading) {
         return (
--- Client/src/pages/employee/Order.jsx
+++ Client/src/pages/employee/Order.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import CheckoutPanel from "../../components/CheckoutPanel";
 import EmployeeNav from "../../components/EmployeeNav";
 import OrderItemPane from "../../components/OrderItemPane";
+import PageProtector from "../../components/PageProtector";
 
 export default function Order(props) {
 
@@ -14,12 +15,12 @@ export default function Order(props) {
 
     */
     const currCart = localStorage.getItem('cart');
-    const [cart, setCart] = useState(currCart ? JSON.parse(currCart) : {total: [0], items: {}});
+    const [cart, setCart] = useState(currCart ? JSON.parse(currCart) : { total: [0], items: {} });
 
     const addToCart = (item, excludeItems) => {
         const cartID = Object.keys(cart.items).length;
         console.log("add to cart clicked ->", item.name, excludeItems, cartID)
-        let newCart = {...cart};
+        let newCart = { ...cart };
         //create entry in cart if it doesn't exist and excluded items is different
         for (let key in newCart.items) {
             if (newCart.items[key][0] === item.name && JSON.stringify(newCart.items[key][4]) === JSON.stringify(excludeItems)) {
@@ -42,14 +43,14 @@ export default function Order(props) {
     }
 
     const emptyCart = () => {
-        setCart({total: [0], items: {}});
-        localStorage.setItem('cart', JSON.stringify({total: [0], items: {}}));
+        setCart({ total: [0], items: {} });
+        localStorage.setItem('cart', JSON.stringify({ total: [0], items: {} }));
     }
 
     const removeFromCart = (cartID) => {
         console.log("remove from cart clicked ->", cartID)
 
-        let newCart = {...cart};
+        let newCart = { ...cart };
         newCart.total[0] -= newCart.items[cartID][2];
         if (newCart.items[cartID][1] > 1) {
             newCart.items[cartID][1] -= 1;
@@ -63,10 +64,12 @@ export default function Order(props) {
 
     const isManager = props.isManager;
     return (
-        <div className="empOrderPage">
-            <EmployeeNav isManager={isManager} current={"order"} />
-            <OrderItemPane addToCart={addToCart}></OrderItemPane>
-            <CheckoutPanel cart={cart} emptyCart={emptyCart} removeFromCart={removeFromCart} />
-        </div >
+        <PageProtector>
+            <div className="empOrderPage">
+                <EmployeeNav isManager={isManager} current={"order"} />
+                <OrderItemPane addToCart={addToCart}></OrderItemPane>
+                <CheckoutPanel cart={cart} emptyCart={emptyCart} removeFromCart={removeFromCart} />
+            </div >
+        </PageProtector>
     );
 }
